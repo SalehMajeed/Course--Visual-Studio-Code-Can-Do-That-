f1da4227d79d0aea386f9db53f50b2b5e3f7c0d2
= <saleh.majeed8@gmail.com>
2020-10-23 15:43:25 +0530

    table_contents

---
+++ table_contents.js
@@ -0,0 +1,11 @@
+/**
+    1. Introduction.
+    2. Customizing the Editor.
+    3. Productivity Tricks.
+    4. Navigation & Refacotring.
+    5. Debugging.
+    6. Docker.
+    7. Remote Development.
+    8. Working With Data.
+    9. Git.
+*/
