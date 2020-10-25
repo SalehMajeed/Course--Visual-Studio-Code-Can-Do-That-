def7ea8bc67e8ebe6934d5febbd8e264b2400644
= <saleh.majeed8@gmail.com>
2020-10-25 13:27:11 +0530

    command

--- commands.js
+++ commands.js
@@ -3,5 +3,8 @@
     2. ctrl+shift+p -> command path.
     3. ctrl+p -> file switcher.
     4. ctrl+` or ctrl + j -> to hide and show terminal.
-    5. ctrl + , ->for open settings.
+    5. ctrl + , -> for open settings.
+    6. ctrl + shift + k -> for delete lines.
+    7. alt + down/up Arrow -> move line.
+    8. alt + shift + down/up Arrow -> duplicates lines.
 */
